77976d8d3527318fccab67310b1a63f8d909469f
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-03 11:08:27 +0000

    Account Repository All and Find Methods - Tests Passed

--- lib/account_repository.rb
+++ lib/account_repository.rb
@@ -1,2 +1,52 @@
 require_relative 'account'
 
+class AccountRepository
+
+  def all
+    sql =' SELECT id, username FROM accounts;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    accounts = []
+
+    result_set.each do |record|
+      account = Account.new
+      account.id = record['id']
+      account.username = record['username']
+
+      accounts << account
+    end
+    return accounts  
+  end
+
+  
+  def find(id)
+    sql = 'SELECT id, username FROM accounts WHERE id = $1;'
+    sql_params = [id]
+
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+    record = result_set[0]
+
+    account = Account.new
+    account.id = record['id']
+    account.username = record['username']
+
+    return account
+  end
+
+  # # Add more methods below for each operation you'd like to implement.
+
+  # def create(account)
+  # # INSERT INTO accounts (username) VALUES ($1);
+  # # No return
+  # end
+
+  # def update(account)
+  #   # UPDATE accounts SET username = $1 WHERE id = $2;
+  #   # Returns nothing, just updates record
+  # end
+
+  # def delete(id)
+  # # DELETE FROM accounts WHERE id = $1;
+  # # Return nothing
+  # end
+end
--- spec/account_repository_spec.rb
+++ spec/account_repository_spec.rb
@@ -1,2 +1,68 @@
 require 'account_repository'
 
+RSpec.describe AccountRepository do
+  
+  def reset_accounts_table
+    seed_sql = File.read('spec/social_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_database' })
+    connection.exec(seed_sql)
+  end
+  
+  describe AccountRepository do
+    before(:each) do 
+      reset_accounts_table
+    end
+  
+    it 'returns a list of accounts' do
+      repo = AccountRepository.new
+
+      accounts = repo.all
+      expect(accounts.length).to eq(2)
+      expect(accounts.first.username).to eq('davidjones')
+    end  
+    
+    it 'finds an account record' do
+      repo = AccountRepository.new
+
+      account = repo.find(1)
+      expect(account.id).to eq('1')
+      expect(account.username).to eq('davidjones')
+    end
+
+      # # 3
+      # # Create a new account record
+      # repo = AccountRepository.new
+
+      # account = Account.new
+      # account.username = 'makers'
+
+      # repo.create(account)
+
+      # account = repo.all
+      # last_account = account.last
+      # last_account.username # => 'makers'
+
+      # # 4
+      # # Deletes an account record
+      # repo = AccountRepository.new
+
+      # repo.delete(1)
+
+      # accounts = repo.all
+      # accounts.length # => 1
+      # accounts.first.id # => 2
+
+      # # 5 
+      # # Updates an account
+
+      # repo = AccountRepository.new
+
+      # account = repo.find(1)
+      # account.username = 'woolydog'
+
+      # repo.update(account)
+
+      # updated_acc = repo.find(1)
+      # updated_acc.username # => 'woolydog'
+  end
+end
